33eb88c4704e914b4eb37075b0ad8e98ed4af124
asror797 <asror797shahobiddinov@gmail.com>
2022-09-21 17:26:59 +0500

    #mke65f4

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 
 
+
+app.get('/',(req,res) => {
+   res.send('ok')
+})
 app.listen(9000,() => {
    console.log('Server is ready at 5000 port ')
 })
